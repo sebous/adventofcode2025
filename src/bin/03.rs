910b12af91036a906e22d4287a03eb5e8fb8eb8b
Sebastian Krepela <sebous@gmail.com>
2025-12-04 13:34:42 +0100

    day 3 smart

--- src/bin/03.rs
+++ src/bin/03.rs
@@ -1,5 +1,3 @@
-use itertools::{Itertools, Permutations};
-
 advent_of_code::solution!(3);
 
 fn parse(input: &str) -> Vec<Vec<u64>> {
@@ -15,26 +13,31 @@ fn parse(input: &str) -> Vec<Vec<u64>> {
 }
 
 fn find_next_highest(row: &Vec<u64>, size: usize) -> u64 {
-    // dbg!(row.iter().combinations(2).unique().collect_vec());
-    let row_max = row
+    let mut combination = vec![];
+    let mut last_index = 0;
+    for _ in 0..size {
+        let mut curr_max = 0;
+        let comb_len = combination.len();
+        row.iter()
+            .enumerate()
+            .skip(if comb_len > 0 { last_index + 1 } else { 0 })
+            .take_while(|(i, _)| *i <= (row.len() - size + comb_len))
+            .for_each(|(i, digit)| {
+                if *digit > curr_max {
+                    curr_max = *digit;
+                    last_index = i;
+                }
+            });
+        combination.push(curr_max);
+    }
+    combination
         .iter()
-        .combinations(size)
-        .unique()
-        .sorted()
-        .last()
-        .map(|group| {
-            group
-                .iter()
-                .fold(String::new(), |mut acc, item| {
-                    acc.push_str(&item.to_string());
-                    acc
-                })
-                .parse::<u64>()
-                .unwrap()
+        .fold(String::new(), |mut acc, digit| {
+            acc.push_str(&digit.to_string());
+            acc
         })
-        .unwrap();
-    dbg!(row_max);
-    row_max
+        .parse()
+        .unwrap()
 }
 
 pub fn part_one(input: &str) -> Option<u64> {
